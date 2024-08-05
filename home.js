8ee5f59666b228ede686718c104383e746d0ce9d
pnzrfst <jxssxp@gmail.com>
2024-08-05 15:59:28 -0300

    subindo os arq

--- home.js
+++ home.js
@@ -1,4 +1,6 @@
 
+
+
 //variaveis arrecadacao
 const valorPassagem = 4.40;
 var totalArrecadado = 0.00;
@@ -9,9 +11,10 @@ var totalArrecadado = 0.00;
 var capacidadeOnibus = 30;
 var lotacaoAtual = 0;
 var esteveVazio = 0;
+var tinhaAlguemPresente = false;
 var esteveLotado = 0;
-var fimDoDia = false;
 var estaLotado = false;
+var totalPassageiros = 0;
 //-- 
 
 
@@ -31,13 +34,13 @@ function atualizarContador(){
         atualizarContador.innerText = `Estamos lotados.`
     }
 }
-
-
+//---------
 function acrescimoPassageiros(){
     if(estaLotado) return;
     
     if(lotacaoAtual < capacidadeOnibus){
         lotacaoAtual++;
+        totalPassageiros++;
         
         atualizarContador();
         calcularVagasRestantes();
@@ -52,24 +55,30 @@ function acrescimoPassageiros(){
         esteveLotado++;
     }
     
+    console.log(esteveLotado)
+    return totalPassageiros;
+
 }
 
 //-------------
-
 function decrescimoPassageiros(){
     if(lotacaoAtual > 0){
         lotacaoAtual--;
+        tinhaAlguemPresente = true;
 
        atualizarContador();
        calcularVagasRestantes();
     }
 
-    if(lotacaoAtual == 0){
+    if(lotacaoAtual == 0 && tinhaAlguemPresente){
         atualizarContador.innerText = `O ônibus está vazio.`;
         esteveVazio++;
+        tinhaAlguemPresente = false;
     }
-}
 
+    console.log(esteveVazio)
+}
+//-------------
 function calcularVagasRestantes(){
     var vagasRestantes = capacidadeOnibus - lotacaoAtual;
 
@@ -89,3 +98,108 @@ function calcularVagasRestantes(){
     
     
 }
+//----------
+
+//criando estrutura do contador ao ''iniciar'' o dia.
+function criarEstruturaContador(){
+    const gerarEstruturaContador = `
+
+                    <div class="infosTopo">
+                            <div id="userIcons">
+                                <img src="./src/assets/images/user.png" alt="simbolo usado em imagens de usuario" width="auto" height="60px">
+                                <a href="#" id="setaFimDia" onclick="gerarRelatorioFimDia()">
+                                    <img src="./src/assets/images/arrow.png" alt"seta para direita" width="auto" height="40px">
+                                </a>
+                            </div>
+                            <section class="informacoes-veiculo-motorista">
+                                    <h3 id="placaVeiculo">Placa: </h3>
+                                    <p>AAABB-000</p>
+                                    <h4 id="nomeMotorista"> <i>Paulo Sergio • Linha Centro-Sta.Luzia</i></h4>
+                            </section>
+                         
+                         <span id="underline"></span>
+                    </div>
+
+                    <div class="contadorInfos">
+                        <h2 id="contadorVagasRestantes"></h2>
+                        <p>O total arrecadado é de: <span id="valorTotalArrecadado">R$ 0</span></p>
+                        <p id="capacidadeAtual">Atualmente, não temos passageiros.</p>
+                    </div>
+                    <div class="contadorInfos-btn">
+                        <button id="botaoAcresc" type="button" onclick="acrescimoPassageiros()">Acrescentar passageiros</button>
+                        <button id="botaoDecresc" type="button" onclick="decrescimoPassageiros()">Diminuir passageiros</button>
+                    </div>`
+    const estruturaContador = document.getElementById('estruturaContador');
+    
+    estruturaContador.innerHTML += gerarEstruturaContador;
+
+    const inicializarContador = document.querySelector('.inicializarContador');
+
+    if(inicializarContador){
+        inicializarContador.style.display = 'none';
+    }
+}
+
+document.getElementById('criarEstrutura').addEventListener('click', criarEstruturaContador);
+
+
+function gerarRelatorioFimDia(){
+    const relatorioFimDia = `
+        <div class="relatorioInfos">
+            <table class="tabela" border="2">
+                    <thead>
+                            <tr>
+                                <th>Ganhos totais: (em R$)</th>
+                                <th>Total de passageiros: </th>
+                                <th>Vezes lotado: </th>
+                                <th>Vezes vazio: </th>
+                            </tr>
+                    </thead>       
+                    
+                    <tbody id="tabelaFimDia">
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                    </tbody>     
+            </table>
+            
+        </div>`
+
+    const relatorioFimDoDia = document.getElementById('relatorioFimDoDia');
+
+    if(relatorioFimDoDia){
+        relatorioFimDoDia.innerHTML = relatorioFimDia;
+        gerarTabela();
+    }
+}
+    
+function gerarTabela(){
+        const dadosTabela = {
+            ganhosTotais: totalArrecadado,
+            totalPassageiros: totalPassageiros,
+            esteveLotado: esteveLotado,
+            esteveVazio: esteveVazio
+        };
+
+        console.log(dadosTabela)
+    
+        const tabelaFimDia = document.getElementById('tabelaFimDia');
+
+        if(tabelaFimDia){
+            const tr = tabelaFimDia.querySelector('tr');
+            
+            if(tr){
+                tr.children[0].textContent =  'R$' + dadosTabela.ganhosTotais.toFixed(2) 
+                tr.children[1].textContent = dadosTabela.totalPassageiros
+                tr.children[2].textContent = dadosTabela.esteveLotado
+                tr.children[3].textContent = dadosTabela.esteveVazio
+            }
+        }
+    
+       
+}
+
+document.getElementById('setaFimDia').addEventListener('click', gerarRelatorioFimDia);
