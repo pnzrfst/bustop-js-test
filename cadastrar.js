5564edb23f967dc1bbf341aedf292aa58cb8efe2
pnzrfst <jxssxp@gmail.com>
2024-09-03 17:53:05 -0300

    criando animations

---
+++ cadastrar.js
@@ -0,0 +1,19 @@
+
+const login = document.getElementById('login');
+
+const dadosUsuario = {
+    usuario: "luiz",
+    senha: "1234"
+}; 
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_usuarios')) ?? [];
+const setLocalStorage = (db_usuarios) => localStorage.setItem('db_usuarios', JSON.stringify(db_usuarios));
+
+
+const cadastrarUsuario = (dadosUsuario) => {
+    const db_usuarios = getLocalStorage();
+    db_usuarios.push(dadosUsuario);
+    setLocalStorage(db_usuarios);
+}
+
+const lerUsuarios = () => getLocalStorage()
